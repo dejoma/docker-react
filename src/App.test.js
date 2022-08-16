fd139e44bd1b01cf2d5d2017b8bd4f2ac96b4eb1
dennis <dennis.stander@wetravel.com>
2022-08-16 09:16:39 +0200

    testes updated

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,8 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/I was/i);
+  const linkElement = screen.getByText(/Dennis/i);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInstanceOf("h1");
 });
